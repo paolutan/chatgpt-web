a8551f1ef815a45807fe22903cdeb11bb31a446b
boristan <boristan@tencent.com>
2023-03-05 22:14:20 +0800

    openai.conf -> openai.env

--- start_wo_nigix.sh
+++ start_wo_nigix.sh
@@ -4,4 +4,4 @@ PROJECT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 docker build -t chatgpt/server $PROJECT_DIR
 
-docker run --name chatgpt-web -dp 80:3002 --env_file $PROJECT_DIR/openai.conf chatgpt/server:latest
+docker run --name chatgpt-web -dp 80:3002 --env_file $PROJECT_DIR/openai.env chatgpt/server:latest
