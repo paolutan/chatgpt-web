33ea5833376904be87f50599dd785bea8d31b579
boristan <boristan@tencent.com>
2023-03-05 22:11:35 +0800

    add script to start w/o nginx

---
+++ start_wo_nigix.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+docker build -t chatgpt/server $PROJECT_DIR
+
+docker run --name chatgpt-web -dp 80:3002 --env_file $PROJECT_DIR/openai.conf chatgpt/server:latest
